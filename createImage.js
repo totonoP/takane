7ed467cc389f06af69a099216ef68633e825c84a
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-15 16:17:40 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -7,10 +7,5 @@ function toDraw() {
           ctx.fillStyle = "green"; //塗り潰し色を緑に
 
           ctx.fillText(txt,10,50);      //テキストを塗り潰しで描画
-          ctx.fillText(txt,10,100,100); //テキストの最大幅を100pxに指定
-
-          ctx.strokeStyle = "blue"; //輪郭線の色を青に
-
-          ctx.strokeText(txt,10,150);     //テキスト輪郭を描画
-          ctx.strokeText(txt,10,200,100);
+          
           }
