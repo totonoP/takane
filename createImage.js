168454f71ddeee28be5637fa3e77d79b519202e4
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-15 13:04:22 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -7,16 +7,10 @@ function onKey() {
 function toDraw() {
           var ctx = document.getElementById("cv").getContext("2d");
 
-          var txt ="JavaScript"; //描画する文字
+          var txt = document.forms.serihu_form.serihu.value; //描画する文字
 
-          ctx.font = "italic 40px Arial"; //フォントにArial,40px,斜体を指定
-          ctx.fillStyle = "green"; //塗り潰し色を緑に
+          ctx.font = "italic 10px Arial"; //フォントにArial,40px,斜体を指定
+          ctx.fillStyle = "white"; //塗り潰し色を緑に
 
           ctx.fillText(txt,10,50);      //テキストを塗り潰しで描画
-          ctx.fillText(txt,10,100,100); //テキストの最大幅を100pxに指定
-
-          ctx.strokeStyle = "blue"; //輪郭線の色を青に
-
-          ctx.strokeText(txt,10,150);     //テキスト輪郭を描画
-          ctx.strokeText(txt,10,200,100); //テキストの最大幅を100pxに指定
           }
