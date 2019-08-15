6d506c571654337b67b59a1d17d186a3e7e2752b
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-15 12:26:49 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -1,1 +1,5 @@
-alert("Hello");
+function onButtonClick() {
+          target = document.getElementById("output");
+          target.innerText = document.forms.id_form1.id_textBox1.value;
+          //target.innerText = document.id_form1.id_textBox1.value;//これでもOK
+        }
