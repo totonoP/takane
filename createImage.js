80cb270f59c9baf4862941ca9d3c59a199fe67cc
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-15 13:00:09 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -7,7 +7,7 @@ function onKey() {
 function toDraw() {
           ctx = document.getElementById("cv").getContext("2d");
 
-          var txt = document.forms.serihu_form.serihu.value;//描画する文字
+          var txt = "suika";//描画する文字
 
           ctx.font = "italic 40px Arial"; //フォントにArial,40px,斜体を指定
           ctx.fillStyle = "green"; //塗り潰し色を緑に
