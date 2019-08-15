3d6645d0b6e529ea0d3a94d50521c33617d27472
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-15 20:17:57 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -3,7 +3,7 @@ function toDraw() {
           var ctx = document.getElementById("cv").getContext("2d");
 
           var txt = document.forms.serihu_form.serihu.value; //描画する文字
-          ctx.font = "italic 40px Arial"; //フォントにArial,40px,斜体を指定
+          ctx.font = "20px Arial"; //フォントにArial,40px,斜体を指定
           ctx.fillStyle = "green"; //塗り潰し色を緑に
 
           ctx.fillText(txt,10,50);      //テキストを塗り潰しで描画
