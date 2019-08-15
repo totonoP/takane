a6bd59a0d5dfe7a2986f1c00428322771269010a
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-15 15:44:28 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -3,6 +3,7 @@ function toDraw() {
           var ctx = document.getElementById("cv").getContext("2d");
 
           var txt = document.forms.serihu_form.serihu.value; //描画する文字
+a
 
           ctx.font = "20px Arial"; //フォントにArial,40px,斜体を指定
           ctx.fillStyle = "black"; //塗り潰し色を黒に
