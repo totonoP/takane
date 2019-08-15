bd097f1e78e8429341542d94262aad8aaf7080a6
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-15 12:35:49 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -1,5 +1,5 @@
-function onButtonClick() {
+function onKey() {
           target = document.getElementById("output");
-          target.innerText = document.forms.id_form1.id_textBox1.value;
+          target.innerText = document.forms.serihu_form.serihu.value;
           //target.innerText = document.id_form1.id_textBox1.value;//これでもOK
         }
