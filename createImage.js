cc2233d6a7d829be804bdf47aa6b899ab0b76e3e
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-18 13:29:32 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -12,7 +12,7 @@ function toDraw() {//文字を画像に描画
 
 function whichselect(e) {//選択中の値を取得
           if(e == 1){var slct = document.getElementsByName('e1');}
-          elseif(e == 2){var slct = document.getElementsByName('e2');}
+          else if(e == 2){var slct = document.getElementsByName('e2');}
           var ans;
     for(var i = 0; i < slct.length; i++){
       if(slct[i].checked) {
@@ -27,7 +27,7 @@ function whichselect(e) {//選択中の値を取得
 function DrawImage(name,n) {
   //2Dコンテキストのオブジェクトを生成する
 if(n == 1){var cvs = document.getElementById('cv');}
-elseif(n == 2){var cvs = document.getElementById('cv2');}
+else if(n == 2){var cvs = document.getElementById('cv2');}
   var ctx = cvs.getContext('2d');
  
   //画像オブジェクトを生成
