64eed69a9efc04fc3bb437b50b16e328c9815288
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-29 23:29:31 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -32,7 +32,7 @@ function toDraw() {//文字を画像に描画
         }
 
         /* contextの現在のフォントスタイルで描画したときの長さを取得 */
-        if (ctx.measureText( tmp + c ).width <= 500){
+        if (ctx.measureText( tmp + c ).width <= 1000){
             /* 指定幅を超えるまでは文字列を繋げていく */
             tmp += c;
         }else{
