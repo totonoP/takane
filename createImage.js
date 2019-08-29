d3511843564121c25487afdf51e5f02cf8d586a3
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-29 23:00:34 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -10,9 +10,49 @@ function toDraw() {//文字を画像に描画
           ctx.font = "bold 50px Arial"; //フォントにArial,40px,太字を指定
           ctx.fillStyle = "white"; //塗り潰し色を緑に
           ctx.fillText(txt2,350,840);
+
+    var len = txt.length; 
+    var strArray = [];
+    var tmp = "";
+    var i = 0;
+
+    if( len < 1 ){
+        //textの文字数が0だったら終わり
+        return strArray;
+    }
+
+    for( i = 0; i < len; i++ ){
+        var c = txt.charAt(i);  //textから１文字抽出
+        if( c == "\n" ){
+            /* 改行コードの場合はそれまでの文字列を配列にセット */
+            strArray.push( tmp );
+            tmp = "";
+
+            continue;
+        }
+
+        /* contextの現在のフォントスタイルで描画したときの長さを取得 */
+        if (ctx.measureText( tmp + c ).width <= 10){
+            /* 指定幅を超えるまでは文字列を繋げていく */
+            tmp += c;
+        }else{
+            /* 超えたら、それまでの文字列を配列にセット */
+            strArray.push( tmp );
+            tmp = c;
+        }
+    }
+
+    /* 繋げたままの分があれば回収 */
+    if( tmp.length > 0 )
+        strArray.push( tmp );
+
+    return strArray;
+}
+	
+	
           
-          for( var lines=txt.split(/\n/), i=0, l=lines.length; l>i; i++ ) {
-	var line = lines[i];
+          for( i=0, l=strArray.length; l>i; i++ ) {
+	var line = strArray[i];
           var addY = 900 + 55 * i ;
 
 	ctx.fillText( line, 350,addY );
