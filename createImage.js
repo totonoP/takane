e54be687e9a7753bdf0192eaee0eee20ed3bb5b1
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-29 22:36:24 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -11,7 +11,7 @@ function toDraw() {//文字を画像に描画
           ctx.fillStyle = "white"; //塗り潰し色を緑に
           ctx.fillText(txt2,350,840);
           
-          for( var lines=txt.split( " " ), i=0, l=lines.length; l>i; i++ ) {
+          for( var lines=txt.split( "\n" ), i=0, l=lines.length; l>i; i++ ) {
 	var line = lines[i];
           var addY = 900 + 55 * i ;
 
