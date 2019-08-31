acbeb45781239885857d49e28671bf765b47f97d
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-31 23:58:15 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -139,7 +139,7 @@ function DL()
 	//canvasをJPEG変換し、そのBase64文字列をhrefへセット
 	a.href = canvas.toDataURL('image/jpeg', 0.85);
 	//ダウンロード時のファイル名を指定
-	a.download = 'download.jpg';
+	a.download = 'takanecom.jpg';
 	//クリックイベントを発生させる
 	a.click();
 }
