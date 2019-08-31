0037513a399cc752ac0e03ae268cc4b25d394a66
ととの <54125893+totonoP@users.noreply.github.com>
2019-08-31 20:30:19 +0900

    Update createImage.js

--- createImage.js
+++ createImage.js
@@ -115,16 +115,24 @@ function DrawImage(name,n,t) {
   for(let i=0; i<asset.length; i++){
     const image1 = await getImagefromCanvas(asset[i]);
     ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
-  }
-	   if (canvas.msToBlob) {
+  }   
+}
+
+
+function download(){
+  var canvas = document.querySelector('#resultImage');
+  var downloadLink = document.getElementById('download_link');
+	
+	concatCanvas('#resultImage', ['#cv','#cv2','#cv3','#cv4']);
+	
+    if (canvas.msToBlob) {
     const blob = canvas.msToBlob();
     window.navigator.msSaveBlob(blob, "takane.png");
-} else {
+    } else {
     downloadLink.href = canvas.toDataURL('image/png');
     downloadLink.download = "takane.png";
     downloadLink.click();
-}
-    
+    }
 }
 
 
